a2edebd89ead9a82f231d1e581c097a6570a9892
yahelargas <yahelargas111@gmail.com>
2020-11-07 14:27:59 +0200

    Add files ,  Preprocessing the data

---
+++ Preprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+import arff
+import tensorflow as tf
+import pandas as pd
+
+file = (open('TrainingDataset.arff'))
+lines = file.read().split('\n')
+inputs = []
+outputs = []
+inputsArray = []
+outputsArray = []
+for line in lines:
+    dataByLines = lines[0]
+    line = np.array(dataByLines.split(','))
+    line = list(map(int, line))
+    inputs = np.array(line[:-1])
+    outputs = np.array(line[len(line)-1])
+    inputsArray.append(inputs)
+    outputsArray.append(outputs)
+
+
+inputsMatrix = np.array(inputsArray)
+outputsMatrix = np.array(outputsArray)
+print(len(inputsArray))
+print(len(outputsArray))
+
