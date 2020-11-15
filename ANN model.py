6cf58c5a9f34f234c172ef2687ce556dbabab4cf
yahelargas <yahelargas111@gmail.com>
2020-11-15 22:38:58 +0200

    working model: 0 accuracy

--- ANN model.py
+++ ANN model.py
@@ -12,9 +12,8 @@ outputs = []
 inputsArray = []
 outputsArray = []
 for line in lines:
-    dataByLines = lines[0]
-    line = np.array(dataByLines.split(','))
-    line = list(map(int, line))
+    line = np.array(line.split(','))
+    line = list(map(lambda x: (int(x) + 1) / 2, line))
     inputs = np.array(line[:-1])
     outputs = np.array(line[len(line) - 1])
     inputsArray.append(inputs)
@@ -24,24 +23,26 @@ for line in lines:
 inputsMatrix = np.array(inputsArray)
 outputsMatrix = np.array(outputsArray)
 
-batchInputs = tf.data.Dataset.from_tensor_slices(inputsMatrix).batch(100)
-batchOutputs = tf.data.Dataset.from_tensor_slices(outputsMatrix).batch(100)
 
 
 
-TrainingData = []
-ValidationData = []
-TestData = []
+
+TrainingData = { "inputs": inputsMatrix[:9000], "outputs": outputsMatrix[:9000] }
+ValidationData = { "inputs": inputsMatrix[9000:10000], "outputs": outputsMatrix[9000:10000] }
+TestData = { "inputs": inputsMatrix[10000:], "outputs": outputsMatrix[10000:] }
 
 #  Divide the data to Training data \ Validation data \ Test data.
-TrainingData.append(inputsMatrix[:9000])
-TrainingData.append(outputsMatrix[:9000])
+TrainingData["inputs"] = tf.data.Dataset.from_tensor_slices(TrainingData["inputs"]).batch(100)
+TrainingData["outputs"] = tf.data.Dataset.from_tensor_slices(TrainingData["outputs"]).batch(100)
+TrainingData["data"] = tf.data.Dataset.zip((TrainingData["inputs"], TrainingData["outputs"]))
 
-ValidationData.append(inputsMatrix[9000:10000])
-ValidationData.append(outputsMatrix[9000:10000])
 
-TestData.append(inputsMatrix[10000:])
-TestData.append(outputsMatrix[10000:])
+ValidationData["inputs"] = tf.data.Dataset.from_tensor_slices(ValidationData["inputs"]).batch(len(ValidationData["inputs"]))
+ValidationData["outputs"] = tf.data.Dataset.from_tensor_slices(ValidationData["outputs"]).batch(len(ValidationData["outputs"]))
+ValidationData["data"] = tf.data.Dataset.zip((ValidationData["inputs"], ValidationData["outputs"]))
+
+TestData["inputs"] = tf.data.Dataset.from_tensor_slices(TestData["inputs"]).batch(len(TestData["inputs"]))
+TestData["outputs"] = tf.data.Dataset.from_tensor_slices(TestData["outputs"]).batch(len(TestData["outputs"]))
 
 # #### Create the ANN model #### #
 INPUTS = 0
@@ -49,20 +50,23 @@ OUTPUTS = 1
 input_size = 30
 output_size = 1
 
-hidden_layer_size = 10
+hidden_layer_size = 100
 
 model = tf.keras.Sequential([
 
     tf.keras.layers.Dense(input_size),  # input layer
     tf.keras.layers.Dense(hidden_layer_size, activation='relu'),  # 1st hidden layer
     tf.keras.layers.Dense(hidden_layer_size, activation='relu'),  # 2nd hidden layer
+    tf.keras.layers.Dense(hidden_layer_size, activation='relu'),  # 3nd hidden layer
+    tf.keras.layers.Dense(hidden_layer_size, activation='relu'),  # 4nd hidden layer
     tf.keras.layers.Dense(output_size, activation='softmax')  # output layer
 ])
 
 # choosing optimizer and loss function
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accurasy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 
 # #### Training the model (unfinished) #### #
-NUM_EPOCHS = 6
-model.fit(TrainingData, epochs=NUM_EPOCHS, validation_data=(ValidationData[INPUTS],ValidationData[OUTPUTS]), verbose=2)
+NUM_EPOCHS = 10
+model.fit(TrainingData["data"], epochs=NUM_EPOCHS, validation_data=ValidationData["data"], verbose=2)
+
