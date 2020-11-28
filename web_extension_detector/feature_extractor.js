8bb4e59f3d9be8c4c49fb40f7585875119ff8b80
yahelargas <yahelargas111@gmail.com>
2020-11-28 22:31:15 +0200

    checked features

--- web_extension_detector/feature_extractor.js
+++ web_extension_detector/feature_extractor.js
@@ -26,13 +26,13 @@ function URLlength(url){
 
 //1.1.3 
 function tinyURL(){
-    tinyurl = [ "bitly.com" , "rebrandly.com" , "short.io" , "linklyhq.com" , "clickmeter.com" , "bl.ink" , "cutt.ly" , "manage.smarturl.it" , "soo.gd" , "tinycc.com" , "clkim.com" , "tinyurl.com" , "pixelme.me" , "t2mio.com" , "tiny.ie" , "shorturl.at" , "bit.do" , "yourls.org" , "musicjet.com" , "adf.ly" , "is.gd" ]
-    let refferer = new URL(document.referrer);
-    if(tinyURL.includes(referrer.hostname) ) //check if the hostname is in the tinyurl list
-        return Phishing
+    let tinyurl = [ "bitly.com" , "rebrandly.com" , "short.io" , "linklyhq.com" , "clickmeter.com" , "bl.ink" , "cutt.ly" , "manage.smarturl.it" , "soo.gd" , "tinycc.com" , "clkim.com" , "tinyurl.com" , "pixelme.me" , "t2mio.com" , "tiny.ie" , "shorturl.at" , "bit.do" , "yourls.org" , "musicjet.com" , "adf.ly" , "is.gd" ]
+    let reffererToCheck = new URL(document.referrer);
+    if(tinyurl.indexOf(reffererToCheck.hostname) > -1 ) //check if the hostname is in the tinyurl list
+        return Legitimate
     
     else
-        return Legitimate
+        return Phishing
 }
 
 
@@ -121,10 +121,12 @@ function internalUrlRequests(){
     var homeHost = (new URL(location.href)).hostname;
     var aElements = document.getElementsByTagName("img"); //get all elemnts in specific tag .
     for (element in aElements){
-        if(element.href.hostname == homeHost) //if same host name of tab and element
+        if(element.href){ //check if the element has herf field
+            if(element.href.hostname == homeHost) //if same host name of tab and element
             internalCounter += 1;
         else
             externalCounter += 1;
+        }
     }
     if(externalCounter / (externalCounter + internalCounter) > 0.61) //pracentage specified in feature docs
         return Phishing;
@@ -139,10 +141,15 @@ function internalUrlRequestsinA(){
     var homeHost = (new URL(location.href)).hostname;
     var aElements = document.getElementsByTagName("a"); //get all elemnts .
     for (element in aElements){
-        if(element.href.hostname == homeHost) //if same host name of tab and element
-            internalCounter += 1;
-        else
-            externalCounter += 1;
+
+        if(element.href){ //checking if  <a> </a> herf  exist
+            let herfOfElement = new URL(element.href)
+            if(herfOfElement.hostname == homeHost) //if same host name of tab and element
+                internalCounter += 1;
+            else
+                externalCounter += 1;
+
+        }
     }
     if(externalCounter / (externalCounter + internalCounter) > 0.67) //pracentage specified in feature docs
         return Phishing;
@@ -159,18 +166,25 @@ function internalUrlRequestsinMetaScriptsLink(){
 
     var elements = document.getElementsByTagName("link"); //get all elemnts of link.
     for (element in elements){
-        if(element.href.hostname == homeHost) //if same host name of tab and element
-            internalCounter += 1;
-        else
-            externalCounter += 1;
+        if(element.href){ //check if herf exist on the elemnt
+            
+            if(element.href.hostname == homeHost) //if same host name of tab and element
+                internalCounter += 1;
+            
+            else
+                externalCounter += 1;
+        }
+
     }
 
     elements = document.getElementsByTagName("script"); //get all elemnts of script.
     for (element in elements){
-        if(new URL (element.src).hostname == homeHost) //if same host name of tab and element
-            internalCounter += 1;
-        else
-            externalCounter += 1;
+        if(element.src){
+            if(new URL (element.src).hostname == homeHost) //if same host name of tab and element
+                internalCounter += 1;
+            else
+                externalCounter += 1;
+        }
     }
 
     elements = document.getElementsByTagName("meta"); //get all elemnts of meta.
