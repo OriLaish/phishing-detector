7613cebfe2c707d01dae290a86c9fa67887588ff
Ori Laish <orilaish1@gmail.com>
2020-12-02 19:14:10 +0200

    added a feature to the final model

--- model_creation/ANN model.py
+++ model_creation/ANN model.py
@@ -16,9 +16,9 @@ data.rename(columns={'Result': 'Class'}, inplace=True)
 data['Class'] = data['Class'].map({-1: 0, 1: 1})
 
 # removing data of unfounded extension data
-list_of_unfounded_features = ['SSLfinal_State', 'Domain_registeration_length','Submitting_to_email', 'Abnormal_URL',
-       'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow', 'Iframe','age_of_domain', 'DNSRecord', 'web_traffic',
-       'Page_Rank', 'Google_Index', 'Links_pointing_to_page', 'Statistical_report']
+list_of_unfounded_features = ['SSLfinal_State', 'Domain_registeration_length', 'Submitting_to_email', 'Abnormal_URL',
+                              'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow', 'age_of_domain', 'DNSRecord',
+                              'web_traffic', 'Page_Rank', 'Google_Index', 'Links_pointing_to_page', 'Statistical_report']
 
 data = data = data.drop(columns=list_of_unfounded_features)  # removing data
 NUM_OF_FEATURES -= len(list_of_unfounded_features)  # updating dimension size
@@ -30,7 +30,6 @@ y_data = data.iloc[0: -1, NUM_OF_FEATURES]  # result data
 # splitting data to training, validation & testing
 x_temp, x_test, y_temp, y_test = skms.train_test_split(x_data, y_data, test_size=0.09, random_state=18)
 x_train, x_val, y_train, y_val = skms.train_test_split(x_temp, y_temp, test_size=0.1, random_state=27)
-
 
 # ###### Creating ANN model ###### #
 HIDDEN_LAYER_SIZE = 100
@@ -46,6 +45,7 @@ model = tf.keras.Sequential([
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.05, patience=5)  # to prevent over fit
 
-model.fit(x_train, y_train, epochs=10, batch_size=100, validation_data=(x_val, y_val), verbose=1, callbacks=[early_stopping])
+model.fit(x_train, y_train, epochs=10, batch_size=100, validation_data=(x_val, y_val), verbose=1,
+          callbacks=[early_stopping])
 
 tfjs.converters.save_keras_model(model, "savedModel")
