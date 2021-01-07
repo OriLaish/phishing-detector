69ba9fdd11938a83dddf4e1f7b6feb64a9433d83
yahelargas <yahelargas111@gmail.com>
2021-01-07 11:18:36 +0200

    print html file

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -1,1 +1,11 @@
 from bs4 import BeautifulSoup
+import requests
+
+#just testing with random url
+urlToSend = "https://labs.vocareum.com/main/main.php?m=editor&asnid=287309&stepid=287310&hideNavBar=1"
+response = requests.get(urlToSend)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+print(type(soup)) #check if the soup type is beautifulsoup
+print(soup.prettify()) #print the html file ,  with all tags.
