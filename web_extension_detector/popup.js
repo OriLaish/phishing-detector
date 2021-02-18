ac47903ff8c5154661e5b5aafe0d06eee5e8707e
yahelargas <yahelargas111@gmail.com>
2021-02-18 16:46:59 +0200

    change "Y" or "N" to true  or false

--- web_extension_detector/popup.js
+++ web_extension_detector/popup.js
@@ -8,7 +8,7 @@ LegitimateElement.addEventListener("click", LegitimateSites);
 
 function PhishingSites() {
     console.log("phishing");
-    chrome.runtime.sendMessage({Is_Phishing: "N" , sender: "popup.js"}, function(response) {
+    chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js"}, function(response) {
         console.log(response);
       });
     
@@ -19,7 +19,7 @@ function PhishingSites() {
 
 function LegitimateSites() {
     console.log("Legitimate");
-    chrome.runtime.sendMessage({Is_Phishing: "Y" , sender: "popup.js"}, function(response) {
+    chrome.runtime.sendMessage({Is_Phishing: true , sender: "popup.js"}, function(response) {
         console.log(response);
       });
     
