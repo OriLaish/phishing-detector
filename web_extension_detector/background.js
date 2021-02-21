568898ecf1551912d614dfb36ba8adaea71c2dd1
yahelargas <yahelargas111@gmail.com>
2021-02-21 17:02:58 +0200

    change var name so sync with server.

--- web_extension_detector/background.js
+++ web_extension_detector/background.js
@@ -58,7 +58,7 @@ const PhishingPradiction = 0.99
                 var ServerURL = "http://127.0.0.1:8000/submit_url"
                 // send it to the server
                 var HTTPRequest = new XMLHttpRequest();
-                var params = 'Is_Phishing='+ Is_PhishingToSend + '&url=' + url  + '&features=' + featuresToSend;
+                var params = 'is_phishing='+ Is_PhishingToSend + '&url=' + url  + '&features=' + featuresToSend;
                 HTTPRequest.open('POST', ServerURL, true);
 
                 //Send the proper header information along with the request
