1c6c6004fd51e91c6e614cbb3fcc4ba64b04f72c
Ori Laish <orilaish1@gmail.com>
2021-03-07 14:54:53 +0200

    added f1 calculation

--- model_creation/ANN model.py
+++ model_creation/ANN model.py
@@ -3,8 +3,19 @@ import pandas as pd
 import sklearn.model_selection as skms
 import tensorflow as tf
 import tensorflowjs as tfjs
+import tensorflow.keras.backend as K
 
 NUM_OF_FEATURES = 30
+
+
+def get_f1(y_true, y_pred): #taken from old keras source code
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
 
 # ###### Data Processing ###### #
 
@@ -32,20 +43,24 @@ x_temp, x_test, y_temp, y_test = skms.train_test_split(x_data, y_data, test_size
 x_train, x_val, y_train, y_val = skms.train_test_split(x_temp, y_temp, test_size=0.1, random_state=27)
 
 # ###### Creating ANN model ###### #
-HIDDEN_LAYER_SIZE = 100
+HIDDEN_LAYER_SIZE = 50
 
 model = tf.keras.Sequential([
     tf.keras.layers.Input(NUM_OF_FEATURES),  # inputs layer
     tf.keras.layers.Dense(HIDDEN_LAYER_SIZE, activation='relu'),  # 1st hidden layer
     tf.keras.layers.Dense(HIDDEN_LAYER_SIZE, activation='relu'),  # 2st hidden layer
     tf.keras.layers.Dense(HIDDEN_LAYER_SIZE, activation='relu'),  # 3st hidden layer
+    tf.keras.layers.Dense(HIDDEN_LAYER_SIZE, activation='relu'),  # 3st hidden layer
+    tf.keras.layers.Dense(HIDDEN_LAYER_SIZE, activation='relu'),  # 3st hidden layer
     tf.keras.layers.Dense(1, activation='sigmoid')  # output layer
 ])
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', get_f1])
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.05, patience=5)  # to prevent over fit
 
 model.fit(x_train, y_train, epochs=10, batch_size=100, validation_data=(x_val, y_val), verbose=1,
           callbacks=[early_stopping])
 
 tfjs.converters.save_keras_model(model, "savedModel")
+
+
