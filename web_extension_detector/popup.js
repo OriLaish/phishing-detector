963e8d39d6e4db5646a01127e0d0410c962ae1c0
yahelargas <yahelargas111@gmail.com>
2021-03-27 15:16:53 +0300

    unchecked with server

--- web_extension_detector/popup.js
+++ web_extension_detector/popup.js
@@ -10,26 +10,30 @@ function PhishingSites() {
   
   chrome.cookies.get({ url: 'http://127.0.0.1:8000', name: 'PhishingDetectorCookie' },
   function (cookie) {
-    if (cookie) { //cookie exist 
+    if (cookie) { //cookie exist
+      alert(cookie.value) 
       chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js" , cookieValue: cookie.value}, function(response) {
         console.log(response);
       });
     }
     else { // need to create the cookie
+      
       var HTTPRequest = new XMLHttpRequest();
       var ServerURL = "http://127.0.0.1:8000/get_uid"
-      HTTPRequest.open('POST', ServerURL, true);
-      //Send the proper header information along with the request
-      HTTPRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
       HTTPRequest.onreadystatechange = function() {//Call a function when the state changes.
-      if(HTTPRequest.readyState == 4 && HTTPRequest.status == 200) {
-          var returnJson = JSON.parse(HTTPRequest.responseText); //get the uid from the server 
-          chrome.cookies.set({ url: "http://127.0.0.1:8000", name: "PhishingDetectorCookie", value: returnJson.uid}); // create the cookie
-        chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js" , cookieValue: returnJson.uid}, function(response) {
-          console.log(response);
-        });
-        } 
-      }
+        if(HTTPRequest.readyState == 4 && HTTPRequest.status == 200) {
+          
+            var returnJson = JSON.parse(HTTPRequest.responseText); //get the uid from the server 
+            chrome.cookies.set({ url: "http://127.0.0.1:8000", name: "PhishingDetectorCookie", value: returnJson.uid}); // create the cookie
+          chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js" , cookieValue: returnJson.uid}, function(response) {
+            console.log(response);
+          });
+          } 
+        }
+      
+      HTTPRequest.open('GET', ServerURL, true);
+      HTTPRequest.send( null )
     }  
 });
 }
@@ -48,18 +52,18 @@ function LegitimateSites() {
     else { // need to create the cookie
       var HTTPRequest = new XMLHttpRequest();
       var ServerURL = "http://127.0.0.1:8000/get_uid"
-      HTTPRequest.open('POST', ServerURL, true);
-      //Send the proper header information along with the request
-      HTTPRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       HTTPRequest.onreadystatechange = function() {//Call a function when the state changes.
-      if(HTTPRequest.readyState == 4 && HTTPRequest.status == 200) {
-          var returnJson = JSON.parse(HTTPRequest.responseText); //get the uid from the server 
-          chrome.cookies.set({ url: "http://127.0.0.1:8000", name: "PhishingDetectorCookie", value: returnJson.uid}); // create the cookie
-          chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js" , cookieValue: returnJson.uid}, function(response) {
-            console.log(response);
-          });
-        } 
-      }
+        if(HTTPRequest.readyState == 4 && HTTPRequest.status == 200) {
+            var returnJson = JSON.parse(HTTPRequest.responseText); //get the uid from the server 
+            chrome.cookies.set({ url: "http://127.0.0.1:8000", name: "PhishingDetectorCookie", value: returnJson.uid}); // create the cookie
+            chrome.runtime.sendMessage({Is_Phishing: false , sender: "popup.js" , cookieValue: returnJson.uid}, function(response) {
+              console.log(response);
+            });
+          } 
+        }
+  
+      HTTPRequest.open('GET', ServerURL, true);
+      HTTPRequest.send( null )
     }   
   });
 
